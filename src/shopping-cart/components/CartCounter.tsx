36a877b4feb6fa67583d33ce4797ea6e02a2a2e3
WildoDev <wildofari99@gmail.com>
2024-11-24 00:37:54 -0300

    Counter Reducer and Accions

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { useAppDispatch, useAppSelector } from '@/store';
+import { addOne, substractOne } from '@/store/counter/counterSlice';
 import { useState } from 'react';
+
 
 interface Props {
   value?: number;
@@ -8,9 +11,12 @@ interface Props {
 
 
 
-export const CartCounter = ({  value = 0 }: Props) => {
+export const CartCounter = ({ value = 0 }: Props) => {
 
-  const [count, setCount] = useState(value);
+  const count = useAppSelector(state => state.counter.count);
+  const dispatch = useAppDispatch();
+
+  // const [count, setCount] = useState(value);
 
   return (
     <>
@@ -18,13 +24,13 @@ export const CartCounter = ({  value = 0 }: Props) => {
 
       <div className="flex">
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={() => dispatch(addOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           +1
         </button>
 
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch(substractOne())}
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           -1
         </button>
--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { count } from 'console';
 
 interface CounterState {
@@ -14,9 +14,21 @@ const counterSlice = createSlice({
     initialState,
     reducers: {
 
+        addOne(state) {
+            state.count++;
+        },
+        substractOne(state) {
+            if (state.count === 0) return;
+            state.count--;
+        },
+        resetCount(state, action: PayloadAction<number>) {
+            if (action.payload < 0) action.payload = 0;
+            state.count = action.payload
+        }
+
     }
 });
 
-export const { } = counterSlice.actions;
+export const { addOne, substractOne, resetCount } = counterSlice.actions;
 
 export default counterSlice.reducer;
