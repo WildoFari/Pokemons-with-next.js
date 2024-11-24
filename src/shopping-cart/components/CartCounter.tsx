1b1e9845405377f0f3f2571630e82ea211b11696
WildoDev <wildofari99@gmail.com>
2024-11-24 01:12:38 -0300

    Server client state

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useAppDispatch, useAppSelector } from '@/store';
-import { addOne, substractOne } from '@/store/counter/counterSlice';
-import { useState } from 'react';
+import { addOne, initCounterState, substractOne } from '@/store/counter/counterSlice';
+import { useEffect } from 'react';
 
 
 interface Props {
@@ -16,7 +16,12 @@ export const CartCounter = ({ value = 0 }: Props) => {
   const count = useAppSelector(state => state.counter.count);
   const dispatch = useAppDispatch();
 
-  // const [count, setCount] = useState(value);
+
+  useEffect(() => {
+    dispatch(initCounterState(value));
+
+  }, [dispatch, value]);
+
 
   return (
     <>
